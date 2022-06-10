761b72dd50eb799c7abc55adbefa155d65e5f58b
Ritu Ataliya <rituataliya08@gmail.com>
2022-06-10 12:00:53 +0000

    W2, Step-2, fetching data from the server

--- portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
@@ -14,5 +14,6 @@ public class HelloWorldServlet extends HttpServlet {
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("text/html;");
     response.getWriter().println("<h1>Hello Ritu!</h1>");
+    
   }
 }
--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -26,3 +26,10 @@ function addRandomGreeting() {
   const greetingContainer = document.getElementById('greeting-container');
   greetingContainer.innerText = greeting;
 }
+
+async function showServerTime() {
+    const responseFromServer = await fetch('/hello');
+    const textFromResponse = await responseFromServer.text();
+    const dateContainer = document.getElementById('m-container');
+    dateContainer.innerText = textFromResponse;
+}
