456c036ae6ab2484f41fc8ca3bf0ea245628f749
Ritu Ataliya <rituataliya08@gmail.com>
2022-06-10 21:20:14 +0000

    W2, Step-3, fetching data from list and displaying a random message

--- portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
@@ -6,14 +6,30 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import java.util.ArrayList;
+import com.google.gson.Gson;
+
 /** Handles requests sent to the /hello URL. Try running a server and navigating to /hello! */
 @WebServlet("/hello")
 public class HelloWorldServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello Ritu!</h1>");
     
-  }
+    ArrayList<String> mylist = new ArrayList<String>(); 
+    mylist.add("Comments:");
+    mylist.add("I like to read");
+    mylist.add("I am learning how to play the piano");
+    mylist.add("My dream city is NYC");
+
+    Gson gson = new Gson();
+    String json_string = gson.toJson(mylist);
+    
+    // Send the JSON as the response
+    response.setContentType("application/json;");
+    response.getWriter().println(json_string);
+
+}
+
+    
 }
